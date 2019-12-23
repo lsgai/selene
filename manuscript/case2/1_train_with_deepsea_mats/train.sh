d9a7ee176fd01289dc2d15b29f68737e8044de2e
Dat Duong <datd1988@gmail.com>
2019-12-22 18:03:54 -0800

    check

--- manuscript/case2/1_train_with_deepsea_mats/train.sh
+++ manuscript/case2/1_train_with_deepsea_mats/train.sh
@@ -15,13 +15,13 @@ python -u ../../../selene_cli.py ./train_deepsea_mat.yml --lr=0.08
 CUDA_VISIBLE_DEVICES=6,7 python3 -u ../../../selene_cli.py train_deepsea_mat_danq_0.001.yml --lr 0.001 > train_deepsea_mat_danq_0.001.log
 
 cd /local/datdb/selene/manuscript/case2/1_train_with_deepsea_mats
-CUDA_VISIBLE_DEVICES=1 python3 -u ../../../selene_cli.py train_deepsea_mat_deepsea2015.yml --lr 0.08 > train_deepsea2017debug.log 
+CUDA_VISIBLE_DEVICES=1 python3 -u ../../../selene_cli.py train_deepsea_mat_deepsea2015.yml --lr 0.08 > train_deepsea2017debug.log
 
 cd /local/datdb/selene/manuscript/case2/1_train_with_deepsea_mats
-CUDA_VISIBLE_DEVICES=1,2 python3 -u ../../../selene_cli.py train_deepsea_mat_BertBase.yml --lr 0.0001 > train_BertBase.log 
+CUDA_VISIBLE_DEVICES=1,2 python3 -u ../../../selene_cli.py train_deepsea_mat_BertBase.yml --lr 0.0001 > train_BertBase.log
 
 
 HELLOOOOO2
 
 
-CONFLICT 
+CONFLICT
