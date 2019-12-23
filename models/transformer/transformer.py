1577a6b36e8bc84ad9375e75f6b64588ff26e757
Dat Duong <datd1988@gmail.com>
2019-12-22 17:42:38 -0800

    Update transformer.py

--- models/transformer/transformer.py
+++ models/transformer/transformer.py
@@ -111,6 +111,8 @@ class BertEmbeddingsLabel(nn.Module):
     embeddings = self.dropout(embeddings)
 
 
+    HELLOOOOO
+
     #self.dropout( self.LayerNorm(self.word_embeddings.weight) ) # only need this, not indexing word_embeddings
 
     return embeddings
